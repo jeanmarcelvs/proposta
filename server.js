80e69716779fa61f0a0496b1d00a78bfc869ef7d
jeanmarcelvs <jeanmarcel.vs@gmail.com>
2025-09-02 11:11:02 -0300

    02092025 1110

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const axios = require('axios'); // Importa o axios
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,20 +14,17 @@ const BCB_API_URL = process.env.BCB_API_URL;
 // Endpoint para buscar a taxa Selic
 app.get('/selic', async (req, res) => {
   try {
-    const response = await fetch(BCB_API_URL);
-
-    if (!response.ok) {
-        throw new Error(`Erro na API do BCB: ${response.statusText}`);
-    }
-    const data = await response.json();
+    // Usa axios para fazer a requisição
+    const response = await axios.get(BCB_API_URL);
+    const data = response.data; // Axios já converte a resposta para JSON
     res.json(data);
   } catch (error) {
-    console.error('Erro ao buscar a Selic:', error);
+    console.error('Erro ao buscar a Selic:', error.message);
     res.status(500).send('Erro ao buscar dados da Selic.');
   }
 });
 
-// Inicia o servidor--------
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor proxy rodando na porta ${PORT}`);
 });
